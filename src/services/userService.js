7e77588474b84d8c48b114812fd7e6ac6b3620af
danhipke <danhipke@yahoo.com>
2017-03-04 22:31:18 -0800

    Removed old action

--- src/services/userService.js
+++ src/services/userService.js
@@ -3,7 +3,6 @@ import request from 'superagent'
 export const CREATE_USER = 'CREATE_USER'
 export const CREATE_USER_SUCCESS = 'CREATE_USER_SUCCESS'
 export const CREATE_USER_FAILURE = 'CREATE_USER_FAILURE'
-export const CHECK_USER_EXISTS = 'CHECK_USER_EXISTS'
 
 export function createUser (value) {
   return {
@@ -30,24 +29,6 @@ const userService = store => next => action => {
         const data = JSON.parse(res.text)
         next({
           type: CREATE_USER_SUCCESS,
-          payload: data
-        })
-      })
-      break
-    }
-    case CHECK_USER_EXISTS: {
-      const userId = action.payload
-      request.get(`/api/users/${userId}`)
-      .end((err, res) => {
-        if (err) {
-          return next({
-            type: GET_RANKING_DATA_FAILURE,
-            payload: err
-          })
-        }
-        const data = JSON.parse(res.text)
-        next({
-          type: GET_RANKING_DATA_SUCCESS,
           payload: data
         })
       })
